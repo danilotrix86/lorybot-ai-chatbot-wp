0a509d96ba608405a5ba618a5668bd36e932ece2
Danilo Vaccalluzzo <danilo.vaccalluzzo@gmail.com>
2023-11-12 10:01:43 +0100

    Update lorybot.php

--- lorybot.php
+++ lorybot.php
@@ -11,8 +11,8 @@
  defined('ABSPATH') or exit;
 
 // Set the server URL as an option
-#update_option('lorybot_server_url', 'https://lorybot.pythonanywhere.com/');
-update_option('lorybot_server_url', 'http://127.0.0.1:5000/');
+update_option('lorybot_server_url', 'https://lorybot.pythonanywhere.com/');
+#update_option('lorybot_server_url', 'http://127.0.0.1:5000/');
 
   
 require_once plugin_dir_path(__FILE__) . 'includes/utils.php';
